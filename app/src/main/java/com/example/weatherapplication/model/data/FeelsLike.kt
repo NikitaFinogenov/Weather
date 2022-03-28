cb505f224a1ea403952d9a31babf6ce4affb1623
finog <finogenov.07@mail.ru>
2022-03-28 19:09:18 +0300

    Добавлен dataclass

---
+++ app/src/main/java/com/example/weatherapplication/model/data/FeelsLike.kt
@@ -0,0 +1,9 @@
+package com.example.weatherapplication.model.data
+
+
+    data class FeelsLike(
+        val day: Float?,
+        val night: Float?,
+        val eve: Float?,
+        val morn: Float?,
+    )
