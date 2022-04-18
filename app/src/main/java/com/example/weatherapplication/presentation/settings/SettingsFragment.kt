42211d441f3b5bfa3be48c5d2e35a2d8a682468b
finog <finogenov.07@mail.ru>
2022-04-18 19:40:49 +0300

    Измененеи параметра расширенности

--- app/src/main/java/com/example/weatherapplication/presentation/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/weatherapplication/presentation/settings/SettingsFragment.kt
@@ -1,12 +1,15 @@
 package com.example.weatherapplication.presentation.settings
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
+import androidx.preference.Preference
+import androidx.preference.PreferenceManager
 import com.example.weatherapplication.R
 import com.example.weatherapplication.databinding.FragmentSettingsBinding
 
@@ -34,7 +37,36 @@ class SettingsFragment : Fragment() {
         binding.buttonSignout.setOnClickListener {
             findNavController().navigate(R.id.action_nav_settings_to_welcomeFragment)
         }
+
+        val prefs = PreferenceManager.getDefaultSharedPreferences(requireContext())
+        val isExpanded = prefs.getBoolean("expanded_weather", false)
+
+        if (isExpanded) binding.switchExpand.isChecked = isExpanded
+
+        binding.switchExpand.setOnCheckedChangeListener { buttonView, isChecked ->
+            val prefs = PreferenceManager.getDefaultSharedPreferences(requireContext())
+            prefs.edit()
+                .putBoolean("expanded_weather", isChecked)
+                .apply()
+
+        }
+
+
+
     }
 
 
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
